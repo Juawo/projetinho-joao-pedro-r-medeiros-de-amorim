05beb8aff3db3347da03f96875c2abe595289258
João Pedro R. Medeiros de Amorim <joaopedrojp2703@gmail.com>
2024-08-13 11:37:34 -0300

    Ajustando/Detalhando funções e adicionando Função de exibir tarefas concluídas

--- funcoesIndex.js
+++ funcoesIndex.js
@@ -2,18 +2,18 @@ let listaTarefas = []
 let listaTarefasConc = []
 
 export function criarTarefa() {
-  let titulo = prompt("Título da tarefa (obrigatório) : ").trim()
-  const descricao = prompt("Descrição da tarefa (opcional) : ").trim()
-  let vencimento = prompt("Vencimento da tarefa (obrigatório) : ").trim()
+  let titulo = prompt("Título da tarefa (obrigatório) : ")
+  let descricao = prompt("Descrição da tarefa (opcional) : ")
+  let vencimento = prompt("Vencimento da tarefa (obrigatório) : ")
   let prioridade = null
   // Obrigatoriedade de Título e Vencimento 
-  while (titulo === "" || vencimento === "") {
+  while (titulo === null || vencimento === null) {
     console.log("Preencha todos os campos obrigatórios!")
-    titulo === ""
-      ? (titulo = prompt("Título da tarefa : ").trim())
+    titulo === null
+      ? (titulo = prompt("Título da tarefa : "))
       : console.log()
-    vencimento === ""
-      ? (vencimento = prompt("Vencimentoo da tarefa : ").trim())
+    vencimento === null
+      ? (vencimento = prompt("Vencimentoo da tarefa : "))
       : console.log()
   }
   // Obrigatoriedade de Prioridade
@@ -37,18 +37,21 @@ export function criarTarefa() {
         console.log("Opção inválida! Tente uma das opções")
     }
   }
+  descricao === null ? descricao = "Sem descrição" : descricao = descricao
 
   let tarefa = {titulo, descricao, vencimento, prioridade }
   return tarefa
 }
+
 export function adicionarTarefa(tarefa) {
   listaTarefas.push(tarefa)
 }
+
 export function removerTarefa(tarefa) {
   const index = listaTarefas.findIndex((t) => t.titulo === tarefa.titulo)
   if (index != -1) {
     const decisao = parseInt(prompt("Deseja remover essa tarefa? Essa ação não poderá ser desfeita.\n 1 - Sim\n 2 - Não"))
-    if (decisao.toLowerCase() === 1) {
+    if (decisao === 1) {
       listaTarefas.splice(index, 1)
       console.log(`Tarefa : ${tarefa.titulo} removida. `)
     } else {
@@ -58,20 +61,22 @@ export function removerTarefa(tarefa) {
     console.log(`Tarefa : ${tarefa.titulo} não encontrada.`)
   }
 }
+
 export function marcarTarefa() {
   for(let tarefa of listaTarefas){
     console.log(`Título : ${tarefa.titulo}`)
   }
-  const opcao = prompt(`Qual tarefa marcar como concluída : `).trim()
+  const opcao = prompt(`Qual tarefa marcar como concluída : `)
   const index = listaTarefas.findIndex((t) => t.titulo === opcao)
   if (index != -1) {
-    listaConcluidas.push(listaTarefas[index])
+      console.log(`Tarefa ${listaTarefas[index].titulo} marcada como concluída!`)
+    listaTarefasConc.push(listaTarefas[index])
     listaTarefas.splice(index,1)
-    console.log(`Tarefa ${listaTarefas[index]} marcada como concluída!`)
   } else {
     console.log(`${opcao} não foi encontrado na lista de tarefas.`)
   }
 }
+
 export function pesquisarTarefaDescricao() {
   const descricao = prompt("Descrição da tarefa : ").trim()
   if (descricao.length >= 3) {
@@ -85,6 +90,7 @@ export function pesquisarTarefaDescricao() {
       }
     }
 }
+
 export function pesquisarTarefaTitulo(){
   const tituloPesq = prompt("Título da tarefa : ").trim()
   const index = listaTarefas.findIndex((tarefa) => tarefa.titulo === tituloPesq)
@@ -94,20 +100,30 @@ export function pesquisarTarefaTitulo(){
     console.log("Tarefa não encontrada.")
   }
 }
+
 export function resumoTarefa() {
   console.log(`Próxima tarefa a concluir : ${listaTarefas[0]}
     Número total de tarefas : ${listaTarefas.length}
     Número de tarefas pendentes : ${listaTarefas.length}
     Número de tarefas concluídas : ${listaTarefasConc.length}`)
 }
+
 export function exibirTarefas(){
-  for (let tarefa of listaTarefas){
+    console.log(` --- Lista de Tarefas --- \n`)
+    for (let tarefa of listaTarefas){
     console.log(`\u2610 Título : ${tarefa.titulo}
-      Descrição : ${tarefa.descricao}
+      Prioridade : ${tarefa.prioridade}
       Vencimento : ${tarefa.vencimento}
-      Prioridade : ${tarefa.prioridade}\n`)
+      Descrição : ${tarefa.descricao}\n`)
   }
 }
 
-adicionarTarefa(criarTarefa())
-exibirTarefas()
+export function exibirTarefasConcluidas(){
+    console.log(` --- Lista de Tarefas Concluídas --- \n`)
+    for (let tarefa of listaTarefasConc){
+    console.log(`\u2611 ${tarefa.titulo}
+      Prioridade : ${tarefa.prioridade}
+      Vencimento : ${tarefa.vencimento}
+      Descrição : ${tarefa.descricao}\n`)
+    }
+}
